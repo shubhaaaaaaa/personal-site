5f3b9bc498f3a6b93df45e11299153ad35a13eb6
shubhaaaaaaa <shubhakhadgi007@gmail.com>
2022-08-15 13:56:13 +0545

    menu toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ const navMenu = document.getElementById("nav-menu"),
 /* Validate if constant exists */
 if (navToggle) {
   navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
+    navMenu.classList.toggle("show-menu");
   });
 }
 
